53144c60ccef0be7292c98a3f22518300bfa3076
Robin Kuck <robin.kuck@sap.com>
2022-01-09 21:12:16 +0100

    Add basic login UI

--- Firebase Chat/ContentView.swift
+++ Firebase Chat/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 64))
+                            .padding()
+                    }
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                    SecureField("Password", text: $password)
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                }
+                .padding()
+            }
+            .navigationBarTitle("Register Account")
+        }
     }
 }
 
