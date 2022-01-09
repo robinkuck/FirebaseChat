16a5b09acf514402949a6bc72139611ecd52de87
Robin Kuck <robin.kuck@sap.com>
2022-01-09 18:47:32 +0100

    Initial Commit

---
+++ Firebase Chat/Firebase_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_ChatApp.swift
+//  Firebase Chat
+//
+//  Created by Kuck, Robin on 09.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
